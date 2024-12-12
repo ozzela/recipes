f73a27c3427c016c145316cfced15a712fce43ce
E <E>
2024-12-12 15:38:11 -0800

    add recipes

---
+++ index.js
@@ -0,0 +1,12 @@
+function showChocolate(){
+    let chocEl = document.getElementById("chocolate")
+    chocEl.hidden = !chocEl.hidden
+}
+function showBanana(){
+    let banaEl = document.getElementById("banana")
+    banaEl.hidden = !banaEl.hidden
+}
+function showMuffin(){
+    let mufEl = document.getElementById("muffin")
+    mufEl.hidden = !mufEl.hidden
+}
